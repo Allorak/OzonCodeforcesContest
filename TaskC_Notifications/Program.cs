267c8e87f81ddee045a0fccab58327d7effe9655
Allorak <allorakspam@gmail.com>
2022-07-03 16:15:50 +0300

    Task C complete

---
+++ TaskC_Notifications/Program.cs
@@ -0,0 +1,3 @@
+﻿var amountOfUsersAndRequests = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var (userAmount, requestAmount) = (amountOfUsersAndRequests[0], amountOfUsersAndRequests[1]);
+var lastMessage = new Dictionary<int,int>();
