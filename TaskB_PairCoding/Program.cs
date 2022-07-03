7e300d624f933bc437dfbab72b90929c004b67ae
Allorak <allorakspam@gmail.com>
2022-07-03 16:15:42 +0300

    Task B complete

---
+++ TaskB_PairCoding/Program.cs
@@ -0,0 +1,31 @@
+﻿var setAmount = int.Parse(Console.ReadLine());
+for (var i = 0; i < setAmount; i++)
+{
+    var employeeAmount = int.Parse(Console.ReadLine());
+    var employeeStrengthLevels = Console.ReadLine().Split().Select(int.Parse).ToList();
+    var employeeNumbers = new List<int>();
+    for (var j = 0; j < employeeStrengthLevels.Count; j++)
+    {
+        employeeNumbers.Add(j+1);
+    }
+
+    while (employeeStrengthLevels.Any())
+    {
+        var bestPairIndex = 1;
+        for (var j = 2; j < employeeStrengthLevels.Count; j++)
+        {
+            var minimumDifference = Math.Abs(employeeStrengthLevels[0] - employeeStrengthLevels[bestPairIndex]);
+            var currentDifference = Math.Abs(employeeStrengthLevels[0] - employeeStrengthLevels[j]);
+            if (currentDifference < minimumDifference)
+                bestPairIndex = j;
+        }
+
+        Console.WriteLine($"{employeeNumbers[0]} {employeeNumbers[bestPairIndex]}");
+        employeeNumbers.RemoveAt(bestPairIndex);
+        employeeNumbers.RemoveAt(0);
+        employeeStrengthLevels.RemoveAt(bestPairIndex);
+        employeeStrengthLevels.RemoveAt(0);
+    }
+
+    Console.WriteLine();
+}
