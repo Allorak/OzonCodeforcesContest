a5650a20e27a46a187e5981f5e24b1f7a4525599
Allorak <allorakspam@gmail.com>
2022-07-03 19:17:54 +0300

    Task E complete

---
+++ TaskE_Report/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var employeeAmount = int.Parse(Console.ReadLine());
+for (var i = 0; i < employeeAmount; i++)
+{
+    var dayAmount = int.Parse(Console.ReadLine());
+    var tasks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    var previousTasks = new HashSet<int> {tasks[0]};
+    var efficientEmployee = true;
+    for (var j = 1; j < tasks.Length; j++)
+    {
+        if (tasks[j] != tasks[j - 1])
+        {
+            if (previousTasks.Contains(tasks[j]))
+                efficientEmployee = false;
+            else
+                previousTasks.Add(tasks[j]);
+        }
+    }
+
+    Console.WriteLine(efficientEmployee ? "YES" : "NO");
+}
