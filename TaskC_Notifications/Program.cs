5ab5cdf293de6e171bfa38b04320aec8db0a7af2
Allorak <allorakspam@gmail.com>
2022-07-03 16:24:45 +0300

    Task C fixed

--- TaskC_Notifications/Program.cs
+++ TaskC_Notifications/Program.cs
@@ -1,3 +1,37 @@
 ﻿var amountOfUsersAndRequests = Console.ReadLine().Split().Select(int.Parse).ToArray();
 var (userAmount, requestAmount) = (amountOfUsersAndRequests[0], amountOfUsersAndRequests[1]);
-var lastMessage = new Dictionary<int,int>();
+var lastMessages = new Dictionary<int,int>();
+var lastMessageNumber = 0;
+for (var i = 0; i < requestAmount; i++)
+{
+    var requestInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    var (requestType, requestedUser) = (requestInfo[0], requestInfo[1]);
+    switch (requestType)
+    {
+        case 1:
+            SendMessage(requestedUser);
+            break;
+        case 2:
+            Console.WriteLine(GetLastMessage(requestedUser));
+            break;
+    }
+}
+
+void SendMessage(int userId)
+{
+    lastMessageNumber++;
+    if (userId != 0)
+    {
+        lastMessages[userId] = lastMessageNumber;
+        return;
+    }
+    for (var i = 0; i < userAmount; i++)
+    {
+        lastMessages[i + 1] = lastMessageNumber;
+    }
+}
+
+int GetLastMessage(int userId)
+{
+    return lastMessages.ContainsKey(userId) ? lastMessages[userId] : 0;
+}
